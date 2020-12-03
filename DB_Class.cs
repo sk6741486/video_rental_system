043d7ce4a5c942058507cc4ca1d94eb3d6e698f2
Simranjit Kaur <sk6741486@gmail.com>
2020-12-04 10:17:53 +1300

    debug

--- DB_Class.cs
+++ DB_Class.cs
@@ -357,6 +357,18 @@ namespace video_rental_system
             }
             return dt;
         }
+        public DataTable popcust_Data()
+        {
+            DataTable dt = new DataTable();
+            QueryString = "select * From popularcustomer";
+            using (da = new SqlDataAdapter(QueryString, Obj_Conn))
+            {
+                Obj_Conn.Open();
+                da.Fill(dt);
+                Obj_Conn.Close();
+            }
+            return dt;
+        }
     }
 }
 
