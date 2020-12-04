e282804f592dc4cec142fd2fac22f18d969b1776
Simranjit Kaur <sk6741486@gmail.com>
2020-12-04 13:39:41 +1300

    comments

--- unittest_videorentalsystem/unittest_vr.cs
+++ unittest_videorentalsystem/unittest_vr.cs
@@ -5,11 +5,11 @@ using video_rental_system;
 namespace unittest_videorentalsystem
 {
     [TestClass]
-    public class unittest_vr
+    public class unittest_vr//name of unit test 
     {
         DB_Class Obj_data = new DB_Class();
         [TestMethod]
-          public void Test_deletecustomer()
+          public void Test_deletecustomer()//test case for testing
            {
             string Message = Obj_data.CustomerDelete();
             Assert.AreEqual("Customer Data Deleted Successfully", Message);
